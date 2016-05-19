0877a28873bda58af9e391aac70b4dcd0a4f5f77
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-05-19 17:47:36 +0200

    MC: lower virtual noice
    cc @regilero

--- salt/loader.py
+++ salt/loader.py
@@ -1393,7 +1393,8 @@ class LazyLoader(salt.utils.lazy.LazyDict):
                     error_reason = ('Exception raised when processing __virtual__ function'
                               ' for {0}. Module will not be loaded {1}'.format(
                                   module_name, exc))
-                    log.error(error_reason)
+                    # XXX: MC: unactivate virtual logging
+                    log.trace(error_reason)
                     virtual = None
                 # Get the module's virtual name
                 virtualname = getattr(mod, '__virtualname__', virtual)
@@ -1405,7 +1406,8 @@ class LazyLoader(salt.utils.lazy.LazyDict):
                     # Some modules might accidentally return None and are
                     # improperly loaded
                     if virtual is None:
-                        log.warning(
+                        # XXX: MC: unactivate virtual logging
+                        log.trace(
                             '{0}.__virtual__() is wrongly returning `None`. '
                             'It should either return `True`, `False` or a new '
                             'name. If you\'re the developer of the module '
